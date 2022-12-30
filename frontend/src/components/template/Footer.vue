f872fb720574358a08a7b6ef456e0431413eb5b0
NikolasDaltro <nikolasdsantos@gmail.com>
2022-12-30 09:03:13 -0300

    estrutura inicial frontend

---
+++ frontend/src/components/template/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="footer">
+    <span>Copyright <strong>Cod3r</strong> © 2015-2022</span>
+  </footer>
+</template>
+
+<script>
+export default {
+    name:'Footer'
+}
+</script>
+
+<style>
+    .footer{
+        grid-area:footer;
+        background-color: #DDD;
+        color: #333;
+
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 10px;
+    }
+
+</style>
---
+++ frontend/src/components/template/Menu.vue
@@ -0,0 +1,21 @@
+<template>
+  <aside class="menu"></aside>
+</template>
+
+<script>
+export default {
+    nome:'Menu'
+}
+</script>
+
+<style>
+    .menu{
+        grid-area: menu;
+        background: linear-gradient(to right, #232526, #414345);
+
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+    }
+
+</style>
