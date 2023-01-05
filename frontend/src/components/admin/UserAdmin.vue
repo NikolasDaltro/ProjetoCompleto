3295c00782db63747de14c3f61f39677cc77aba5
NikolasDaltro <nikolasdsantos@gmail.com>
2023-01-05 15:00:35 -0300

    cadastro de usuario

--- frontend/src/components/admin/UserAdmin.vue
+++ frontend/src/components/admin/UserAdmin.vue
@@ -5,8 +5,31 @@
 </template>
 
 <script>
+import { baseApiUrl } from '../../global'
+import axios from 'axios'
+
+
 export default {
-    name: 'UserAdmin'
+    name: 'UserAdmin',
+    data: function(){
+        return{
+            mode: 'save',
+            user: {},
+            users: []
+        }
+    },
+    methods:{
+        loadUsers(){
+            const url = `${baseApiUrl}/users`
+            axios.get(url).then(res =>{
+                this.user = res.data
+                console.log(this.user)
+            })
+        }
+    },
+    mounted(){
+        this.loadUsers()
+    }
 }
 </script>
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,7 +12,7 @@ Vue.config.productionTip = false
 
 //TEMPORARIO
 
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NSwibmFtZSI6Ikl1cnkiLCJlbWFpbCI6Im1hbmluQGdtYWlsLmNvbSIsImFkbWluIjpmYWxzZSwiaWF0IjoxNjcyOTIyODQ0LCJleHAiOjE2NzMxODIwNDR9.t_gdtBHau7ZapDxSdpLel_ZcXPUSwfhZ9JtxMPvTpik'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ik5pa29sYXMgRGFsdHJvIiwiZW1haWwiOiJuaWtvbGFzZHNhbnRvc0Bob3RtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2NzI5NDExMTcsImV4cCI6MTY3MzIwMDMxN30.79A1MFRT13CNk2LNN8yAC6HdxRCUVmLX5K0GEyYLTJc'
 
 new Vue({
   store,
