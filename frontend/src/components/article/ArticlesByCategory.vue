e9400c034f79c7d58cd814fd414e57a4d4ff16b3
NikolasDaltro <nikolasdsantos@gmail.com>
2023-01-17 16:13:55 -0300

    artigo por categoria

---
+++ frontend/src/components/article/ArticlesByCategory.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="articles-by-category">
+     <PageTitle icon="fa fa-folder-o"
+        :main="category.name" sub="Categoria" />
+  </div>
+</template>
+
+<script>
+import { baseApiUrl } from '@/global'
+import axios from 'axios'
+import PageTitle from '../template/PageTitle.vue'
+
+export default {
+    name: 'ArticlesByCategory',
+    components: { PageTitle },
+    data: function(){
+        return{
+            category: {},
+            articles: [],
+            page: 1,
+            loadMore: true
+        }
+    },
+    methods: {
+        getCategory(){
+            const url = `${baseApiUrl}/categories/${this.category.id}`
+            axios(url).then(res => this.category = res.data)
+        }
+    },
+    mounted(){
+        this.category.id = this.$route.params.id
+        this.getCategory()
+    }
+}
+</script>
+
+<style>
+
+</style>
