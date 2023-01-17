3a54b26ee0eb77e44e00fb3aa68e447b9db17789
NikolasDaltro <nikolasdsantos@gmail.com>
2023-01-16 21:13:53 -0300

    correção article

--- backend/api/article.js
+++ backend/api/article.js
@@ -18,13 +18,13 @@ module.exports = app => {
         }
 
         if(article.id) {
-            app.db('article')
+            app.db('articles')
                 .update(article)
                 .where({ id: article.id })
                 .then(_ => res.status(204).send())
                 .catch(err => res.status(500).send(err))
         } else {
-            app.db('article')
+            app.db('articles')
                 .insert(article)
                 .then(_ => res.status(204).send())
                 .catch(err => res.status(500).send(err))
@@ -33,7 +33,7 @@ module.exports = app => {
 
     const remove = async (req, res) => {
         try {
-            const rowsDeleted = await app.db('article')
+            const rowsDeleted = await app.db('articles')
                 .where({ id: req.params.id }).del()
             
             try {
@@ -52,10 +52,10 @@ module.exports = app => {
     const get = async (req, res) => {
         const page = req.query.page || 1
 
-        const result = await app.db('article').count('id').first()
+        const result = await app.db('articles').count('id').first()
         const count = parseInt(result.count)
 
-        app.db('article')
+        app.db('articles')
             .select('id', 'name', 'description')
             .limit(limit).offset(page * limit - limit)
             .then(articles => res.json({ data: articles, count, limit }))
@@ -63,7 +63,7 @@ module.exports = app => {
     }
 
     const getById = (req, res) => {
-        app.db('article')
+        app.db('articles')
             .where({ id: req.params.id })
             .first()
             .then(article => {
@@ -79,7 +79,7 @@ module.exports = app => {
         const categories = await app.db.raw(queries.categoryWithChildren, categoryId)
         const ids = categories.rows.map(c => c.id)
 
-        app.db({a: 'article', u: 'users'})
+        app.db({a: 'articles', u: 'users'})
             .select('a.id', 'a.name', 'a.description', 'a.imageUrl', { author: 'u.name' })
             .limit(limit).offset(page * limit - limit)
             .whereRaw('?? = ??', ['u.id', 'a.userId'])
