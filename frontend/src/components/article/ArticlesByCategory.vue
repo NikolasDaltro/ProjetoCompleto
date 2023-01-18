aaa38a82c4016fa5fcc035e3a667cda24583dbfa
NikolasDaltro <nikolasdsantos@gmail.com>
2023-01-18 17:48:52 -0300

    artigo categoria

--- frontend/src/components/article/ArticlesByCategory.vue
+++ frontend/src/components/article/ArticlesByCategory.vue
@@ -4,7 +4,7 @@
         :main="category.name" sub="Categoria" />
      <ul>
         <li v-for="article in articles" :key="article.id" >
-            {{ article.name }}
+            <ArticleItem :article="article" />
         </li>
      </ul>
      <div class="load-more">
@@ -19,10 +19,11 @@
 import { baseApiUrl } from '@/global'
 import axios from 'axios'
 import PageTitle from '../template/PageTitle.vue'
+import ArticleItem from './ArticleItem.vue'
 
 export default {
     name: 'ArticlesByCategory',
-    components: { PageTitle },
+    components: { PageTitle, ArticleItem },
     data: function(){
         return{
             category: {},
