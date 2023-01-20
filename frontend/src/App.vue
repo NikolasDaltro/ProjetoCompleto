a72263659980f0e3ee303e27f9a416ea05bcdedd
NikolasDaltro <nikolasdsantos@gmail.com>
2023-01-20 19:04:00 -0300

    validar admin

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,7 +4,8 @@
 		:hideToggle="!user"
 		:hideUserDropdown="!user" />
 		<Menu v-if="user" />
-		<Content />
+		<Loading v-if="validatingToken" />
+		<Content v-else />
 		<Footer />
 	</div>
 </template>
@@ -17,12 +18,13 @@ import Header from "./components/template/Header"
 import Menu from "./components/template/Menu"
 import Content from "./components/template/Content"
 import Footer from "./components/template/Footer"
+import Loading from "./components/template/Loading"
 
 
 
 export default {
 	name: "App",
-	components: {Header, Menu, Content, Footer},
+	components: {Header, Menu, Content, Footer, Loading},
 	computed: mapState(['isMenuVisible', 'user']),
 	data: function(){
 		return{
@@ -35,7 +37,7 @@ export default {
 
 			 const json = localStorage.getItem(userKey)
 			 const userData = JSON.parse(json)
-			 this.$store.dispatch('setUser', null)
+			 this.$store.commit('setUser', null)
 
 			 if(!userData){
 				this.validatingToken = false
--- frontend/src/config/router.js
+++ frontend/src/config/router.js
@@ -7,6 +7,8 @@ import ArticleByCategory from '../components/article/ArticlesByCategory'
 import ArticleById from '../components/article/ArticleById'
 import Auth from '../components/auth/Auth'
 
+import { userKey } from '../global'
+
 Vue.use(VueRouter)
 
 const routes = [{
@@ -16,7 +18,8 @@ const routes = [{
 },{
     name: 'adminPages',
     path: '/admin',
-    component: AdminPages
+    component: AdminPages,
+    meta: {requiresAdmin: true }
 },{
     name: 'articlesByCategory',
     path: '/categories/:id/articles',
@@ -31,8 +34,20 @@ const routes = [{
     component: Auth
 }]
 
-export default new VueRouter({
+ const router = new VueRouter({
     mode: 'history',
     routes: routes
 })
 
+router.beforeEach((to, from, next) =>{
+    const json = localStorage.getItem(userKey)
+    
+    if(to.matched.some(record => record.meta.requiresAdmin)){
+        const user = JSON.parse(json)
+        user && user.admin ? next() : next({ path: '/'})
+    }else {
+        next()
+    }
+})
+
+export default router
